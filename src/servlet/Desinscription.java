758b089144aee2c6f8364ce1d64eb42a90a83c6a
admin <admin@192.168.10.124>
2017-04-06 17:03:07 +0200

    Création servlet DESINSCRIPTION (à compléter)

---
+++ src/servlet/Desinscription.java
@@ -0,0 +1,34 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/Desinscription")
+public class Desinscription extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public Desinscription() {
+        super();
+
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatch = request.getRequestDispatcher("WEB-INF/views/Desinscription.jsp");
+		dispatch.forward(request, response);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
